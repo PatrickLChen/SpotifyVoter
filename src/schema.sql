ba53b1920176c485c130cf62e2a8490cd7151a42
Patrick Chen <pchen@hubspot.com>
2018-08-29 16:06:23 -0400

    updated tables

--- src/schema.sql
+++ src/schema.sql
@@ -3,19 +3,25 @@ DROP TABLE IF EXISTS room;
 DROP TABLE IF EXISTS room_user;
 
 CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id INTEGER AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE room (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    id INTEGER AUTOINCREMENT,
+    creator_id INTEGER NOT NULL
+    name TEXT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (creator_id) REFERENCES user (id)
 );
 
 CREATE TABLE room_user (
     room_id INTEGER NOT NULL,
-    user_id INTEGER NOT NULL
+    user_id INTEGER NOT NULL,
+    PRIMARY KEY (room_id, user_id)
 );
 
 
